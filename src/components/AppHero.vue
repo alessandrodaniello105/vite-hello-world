607be2703e53c969d9a078ea9cdb949532ed2871
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-09 14:06:31 +0200

    [add]: AppHero components

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'AppHero'
+}
+</script>
+
+<template>
+
+  <div class="img-box">
+    <img src="../assets/img/jumbotron.png" alt="">
+  </div>
+
+</template>
+
+<style scoped>
+
+  .img-box {
+    height: 80vh;
+    text-align: center;
+  }
+
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+
+</style>
